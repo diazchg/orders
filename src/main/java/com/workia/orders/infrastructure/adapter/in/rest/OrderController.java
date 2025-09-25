506b2f8fd717b901947075b90ef9a34617dcbd27
Gustavo Diaz <diazchg@gmail.com>
2025-09-24 23:29:14 -0300

    wip: added failing test of create an order

---
+++ src/main/java/com/workia/orders/infrastructure/adapter/in/rest/OrderController.java
@@ -0,0 +1,4 @@
+package com.workia.orders.infrastructure.adapter.in.rest;
+
+public class OrderController {
+}
---
+++ src/test/java/com/workia/orders/infrastructure/adapter/in/rest/OrderControllerTest.java
@@ -0,0 +1,36 @@
+package com.workia.orders.infrastructure.adapter.in.rest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.net.URI;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class OrderControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        this.objectMapper = new ObjectMapper();
+    }
+
+
+    @Test
+    void validOrderCreated() throws Exception {
+        mockMvc.perform(post(new URI("/orders")))
+                .andExpect(status().isCreated())
+                .andExpect(content().json("{}"));
+    }
+}
