756a33343e4ed2451cfc4c801b99548881307432
Gustavo Diaz <diazchg@gmail.com>
2025-09-25 01:22:40 -0300

    fixed a bug with creted order

--- src/main/java/com/workia/orders/infrastructure/adapter/out/persistence/OrderRepositoryAdapter.java
+++ src/main/java/com/workia/orders/infrastructure/adapter/out/persistence/OrderRepositoryAdapter.java
@@ -3,16 +3,23 @@ package com.workia.orders.infrastructure.adapter.out.persistence;
 import com.workia.orders.application.ports.out.OrderRepositoryPort;
 import com.workia.orders.domain.model.CalculatedOrder;
 import com.workia.orders.infrastructure.adapter.out.persistence.entity.CreatedOrderEntity;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @Repository
 public class OrderRepositoryAdapter implements OrderRepositoryPort {
 
-    private static HashMap<String, CreatedOrderEntity>dbMap;
+    private static Map<String, CreatedOrderEntity> dbMap;
+
+    @PostConstruct
+    private void initializeDB(){
+        OrderRepositoryAdapter.dbMap = new HashMap<>();
+    }
 
     @Override
     public CreatedOrderEntity saveOrder(CalculatedOrder order) {
@@ -22,6 +29,7 @@ public class OrderRepositoryAdapter implements OrderRepositoryPort {
                 .totalAmount(order.getTotalAmount())
                 .uuid(uuid.toString())
                 .build();
-        return OrderRepositoryAdapter.dbMap.put(createdOrderEntity.getUuid(), createdOrderEntity);
+        OrderRepositoryAdapter.dbMap.put(createdOrderEntity.getUuid(), createdOrderEntity);
+        return createdOrderEntity;
     }
 }
